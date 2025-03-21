41bf87e89c0504d13c8016860b7e70bbc0851753
RomanIlechko <72658585+RomanIlechko@users.noreply.github.com>
2025-03-21 16:17:07 +0200

    cleaning donut.py

--- donut.py
+++ donut.py
@@ -5,42 +5,6 @@ from utils import load_image
 from transformers import DonutProcessor, VisionEncoderDecoderModel
 from datasets import load_dataset
 
-
-# img_path = "/Users/romanilechko/Sams/Demo_app/synth_data/datasets/synt_db_images/0_page.JPG"
-# model_path = "models/Other/donut"
-# processor = DonutProcessor.from_pretrained(model_path)
-# model = VisionEncoderDecoderModel.from_pretrained(model_path)
-
-# device = "cpu"
-# model.to(device)
-# # load document image from the DocVQA dataset
-# image = load_image(img_path)
-
-# # prepare decoder inputs
-# task_prompt = "<s_docvqa><s_question>{user_input}</s_question><s_answer>"
-# question = "What is Aristarco Anitz current job?"# "What is Aristarco Anitz current job?"
-# prompt = task_prompt.replace("{user_input}", question)
-# decoder_input_ids = processor.tokenizer(prompt, add_special_tokens=False, return_tensors="pt").input_ids
-
-# pixel_values = processor(image, return_tensors="pt").pixel_values
-
-# outputs = model.generate(
-#     pixel_values.to(device),
-#     decoder_input_ids=decoder_input_ids.to(device),
-#     max_length=model.decoder.config.max_position_embeddings,
-#     pad_token_id=processor.tokenizer.pad_token_id,
-#     eos_token_id=processor.tokenizer.eos_token_id,
-#     use_cache=True,
-#     bad_words_ids=[[processor.tokenizer.unk_token_id]],
-#     return_dict_in_generate=True,
-# )
-
-# sequence = processor.batch_decode(outputs.sequences)[0]
-# sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
-# sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
-# print(processor.token2json(sequence))
-
-
 class DonutModel:
     def __init__(self, model_path):
         self.model_path = model_path
